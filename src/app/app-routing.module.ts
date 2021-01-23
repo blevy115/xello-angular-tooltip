0304a036d15062eadb563bc80845a29f87db87b7
Brandon Levy <brandon.levy@mail.mcgill.ca>
2021-01-23 15:57:47 -0500

    Setup Route for Tooltips Page and redirect all routes to Tooltips Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TooltipsPage } from './pages/tooltips/tooltips.page'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', 
+    redirectTo: 'tooltips', 
+    pathMatch:'full'
+  },
+  {
+    path: 'tooltips',
+    component: TooltipsPage
+  },
+  {
+    path: '**', 
+    redirectTo: 'tooltips', 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
