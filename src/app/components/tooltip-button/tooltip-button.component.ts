2d4a4a064594d8695798e4ea14515a238ef8c22e
Brandon Levy <brandon.levy@mail.mcgill.ca>
2021-01-23 18:24:55 -0500

    Built tooltip buttons and connected components to page with toggling functionality

--- src/app/components/tooltip-button/tooltip-button.component.ts
+++ src/app/components/tooltip-button/tooltip-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-tooltip-button',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TooltipButtonComponent implements OnInit {
 
+  @Input() tooltip: any;
+  @Output() toggleTooltips = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public toggleTooltip(): void{
+    this.toggleTooltips.emit(this.tooltip.id);
+  };
 
 }
--- src/app/pages/tooltips/tooltips.page.ts
+++ src/app/pages/tooltips/tooltips.page.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tooltips.page.sass']
 })
 export class TooltipsPage implements OnInit {
+  
+  tooltips: any[] = [ // Sets Tooltip info
+    {id: 0, buttonText:'Button 1', tooltipText:'Tooltip for Button 1', open: false}, 
+    {id: 1, buttonText:'Button 2', tooltipText:'Tooltip for Button 2', open: false}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toggleTooltips(tooltipId : number){
+    this.tooltips.map(tooltip => {
+      if (tooltip.id == tooltipId) {
+        tooltip.open = !tooltip.open;
+      } else {
+        tooltip.open = false;
+      }
+    });
+  }
 }
