0b3fe0136ee0b95ec693887f856379b2bdb8a033
Brandon Levy <brandon.levy@mail.mcgill.ca>
2021-01-23 19:00:10 -0500

    Made Tooltips close on clicking outside of them and on escape key press

--- src/app/pages/tooltips/tooltips.page.ts
+++ src/app/pages/tooltips/tooltips.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-tooltips',
@@ -11,6 +11,21 @@ export class TooltipsPage implements OnInit {
     {id: 0, buttonText:'Button 1', tooltipText:'Tooltip for Button 1', open: false}, 
     {id: 1, buttonText:'Button 2', tooltipText:'Tooltip for Button 2', open: false}
   ];
+
+  // Determines if Mouse click is in or outside tooltips
+  @HostListener('document:click', ['$event'])
+  documentClick(event: MouseEvent) {
+      let clickedElement = (event.target as Element);
+      if (clickedElement.className != 'tooltip-text' && clickedElement.className != 'tooltip-button') {
+        this.toggleTooltips(-1); // close all tooltips
+      }
+  }
+
+  // Close tooltips on Escape key press
+  @HostListener('document:keydown.escape', ['$event']) 
+  onKeydownHandler(event: KeyboardEvent) {
+    this.toggleTooltips(-1);
+  }
 
   constructor() { }
 
