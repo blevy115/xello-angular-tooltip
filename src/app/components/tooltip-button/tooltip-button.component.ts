53fbf6b83d2f495c17c9ca3f8f0f055e8c4ecdd7
Brandon Levy <brandon.levy@mail.mcgill.ca>
2021-01-25 15:27:29 -0500

    Made tooltip position switch on scroll if edge of screen is touched by tooltip

--- src/app/components/tooltip-button/tooltip-button.component.ts
+++ src/app/components/tooltip-button/tooltip-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tooltip-button',
@@ -9,6 +9,20 @@ export class TooltipButtonComponent implements OnInit {
 
   @Input() tooltip: any;
   @Output() toggleTooltips = new EventEmitter<number>();
+
+  @ViewChild('tooltipButton', {static: false}) private tooltipButton: ElementRef<HTMLDivElement>;
+  @ViewChild('tooltipText', {static: false}) private tooltipText: ElementRef<HTMLDivElement>;
+
+  tooltipOnTop: boolean = true;
+
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView(){
+    if (this.tooltipButton && this.tooltipText){
+      const rect = this.tooltipButton.nativeElement.getBoundingClientRect();
+      const tootlTipHeight = this.tooltipText.nativeElement.offsetHeight + 10 // height of tooltip Text Element + distance bewteen button and tooltip
+      this.tooltipOnTop = rect.top > tootlTipHeight;
+    }
+  }
 
   constructor() { }
 
